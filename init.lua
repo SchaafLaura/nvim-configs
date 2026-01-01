832602af116ffcd69dc169207bb4f87cf89a440d
SchaafLaura <schaaf.laura92@gmail.com>
2026-01-01 15:13:55 +0100

    being a baby and having a visual package manager

--- init.lua
+++ init.lua
@@ -1,121 +1,45 @@
--- This file is in this location:
---     "~/.config/nvim/"
-
-------------------------------------------
---                IDK                   --
-------------------------------------------
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
-------------------------------------------
---               Packages               --
-------------------------------------------
-vim.iter {
-    {
-        source = "nvim-treesitter/nvim-treesitter",
-        checkout = "main",
-        hooks = {
-            post_checkout = function() vim.cmd "TSUpdate" end
-        },
-    },
-    "neovim/nvim-lspconfig",
-    "Mofiqul/dracula.nvim",
-    "seblyng/roslyn.nvim",
-    -- "OmniSharp/omnisharp-vim",
-}:each(require("mini.deps").add)
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
 
--- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
-local path_package = vim.fn.stdpath("data") .. "/site/"
-local mini_path = path_package .. "pack/deps/start/mini.nvim"
-if not vim.loop.fs_stat(mini_path) then
-    vim.cmd([[echo "Installing mini.nvim" | redraw]])
-    local clone_cmd = {
-        "git", "clone", "--filter=blob:none",
-        "https://github.com/nvim-mini/mini.nvim", mini_path
-    }
-    vim.fn.system(clone_cmd)
-    vim.cmd("packadd mini.nvim | helptags ALL")
-    vim.cmd([[echo "Installed mini.nvim" | redraw]])
-    end
-    require("mini.deps").setup {
-        path = {
-            package = path_package
-        }
-    }
+vim.opt.rtp:prepend(lazypath)
 
-------------------------------------------
---             Package-Setup            --
-------------------------------------------
--- Treesitter
-local trans = setmetatable({
-    cs = "c_sharp"
-}, { __index = function (tbl, key) return key end})
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "*",
-    callback = function(e)
-    local ts = require "nvim-treesitter"
-    local has = function (what)
-    return function (it)
-    return it == what
-    end
-    end
-    local parser = trans[e.match]
-    if vim.iter(ts.get_installed()):any(has(parser)) then
-        vim.treesitter.start()
-        if #vim.treesitter.query.get_files(parser, "indents") > 0 then
-            vim.opt.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-            end
-            return
-            end
-            if vim.iter(ts.get_available()):any(has(parser)) then
-                vim.cmd.TSInstall(parser)
-                end
-                end,
-})
+local lazy_config = require "configs.lazy"
 
--- Setups
-require("mini.completion").setup()
-require("mini.snippets").setup()
-require("mini.icons").setup()
-MiniIcons.tweak_lsp_kind()
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
 
-------------------------------------------
---                  LSP                 --
-------------------------------------------
+  { import = "plugins" },
+}, lazy_config)
 
- vim.lsp.enable({
-    -- "csharp_ls"
-    -- "omnisharp"
-    "roslyn"
- })
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
 
-vim.lsp.config("roslyn", {
-    on_attach = function()
-    print("This will run when the server attaches!")
-    end,
-    settings = {
-        ["csharp|inlay_hints"] = {
-            csharp_enable_inlay_hints_for_implicit_object_creation = true,
-            csharp_enable_inlay_hints_for_implicit_variable_types = true,
-        },
-        ["csharp|code_lens"] = {
-            dotnet_enable_references_code_lens = true,
-        },
-    },
-})
+require "options"
+require "autocmds"
 
-------------------------------------------
---              Visuals                 --
-------------------------------------------
+vim.schedule(function()
+  require "mappings"
+end)
+
 
 -- cursors
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
--- theme
-vim.cmd.colorscheme("dracula")
-
--- bar on the left
--- TODO: absolute number instead of "0" in the center
 vim.wo.relativenumber = true
 
 -- indents
@@ -126,24 +50,10 @@ vim.opt.tabstop = 4
 vim.opt.listchars = [[space:·,trail:·,nbsp:+,tab:-  ,precedes:←,extends:→,leadmultispace:·,nbsp:␣]]
 vim.opt.list = true
 
-------------------------------------------
---                Keys                  --
-------------------------------------------
-
--- jump around errors
-vim.keymap.set("n", "]d", function() vim.diagnostic.jump({count = 1, float = true}) end)
-vim.keymap.set("n", "[d", function() vim.diagnostic.jump({count = -1, float = true}) end)
-
 -- display all errors in new thingy
 vim.keymap.set("n", "<leader>q",
                function()
-                    vim.diagnostic.setqflist()
-                    vim.cmd("copen")
-                end)
-
-
-
-
-
-
+               vim.diagnostic.setqflist()
+               vim.cmd("copen")
+               end)
 
---
+++ lua/plugins/init.lua
@@ -0,0 +1,75 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "hyprlang",
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+
+        -- !
+        "c_sharp",
+        "razor"
+      },
+    },
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+      },
+      ensure_installed = {
+        "lua-language-server",
+
+        "xmlformatter",
+        "csharpier",
+        "prettier",
+
+        "stylua",
+        "bicep-lsp",
+        "html-lsp",
+        "css-lsp",
+        "eslint-lsp",
+        "typescript-language-server",
+        "json-lsp",
+        "rust-analyzer",
+
+        -- !
+        "roslyn",
+        -- "csharp-language-server",
+        -- "omnisharp",
+      },
+    },
+  },
+
+  {
+    "seblyng/roslyn.nvim",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    ft = { "cs", "razor" },
+    opts = {
+      -- your configuration comes here; leave empty for default settings
+    },
+  },
+
+}
